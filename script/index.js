878492cc01d780a55c13a46c0fdb674b0eb69d68
joshua.burkhan <bai21030@byui.edu>
2025-01-11 19:05:10 -0500

    final index

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
 document.getElementById("last-modified").textContent = document.lastModified;
+
+//Nav mobil
+const navigation = document.querySelector('.menu');
+const menuBut = document.querySelector('#menu');
+const firstPag = document.querySelector('.first-pag');
+
+menuBut.addEventListener('click', () => {
+  navigation.style.display = navigation.style.display === 'flex' ? 'none' : 'flex';
+  firstPag.style.marginTop = firstPag.style.marginTop === '40px' ? '0' : '40px';
+});
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
